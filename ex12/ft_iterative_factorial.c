43f2d11e37f1344444b90f4191074c4ce4b95e55
Foka tatsinkou Robert bright <fokrober@e1r11p1.1337.ma>
2019-03-28 23:52:10 +0100

    ft_iterative_fatorial.c

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -6,17 +6,20 @@
 /*   By: fokrober <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/28 05:32:11 by fokrober          #+#    #+#             */
-/*   Updated: 2019/03/28 05:41:16 by fokrober         ###   ########.fr       */
+/*   Updated: 2019/03/28 23:46:53 by fokrober         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_iterative_factorial(int nb)
 {
+	int i;
+	
 	if (nb < 0)
 		return (0);
 	if (nb == 1 || nb == 0)
 		return (1);
-	while (nb > 1)
-		nb *= nb - 1;
+	i = nb;
+	while (--i)
+		nb *= i;
 	return (nb);
 }

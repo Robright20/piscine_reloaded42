ae140214c33215d1c6cae63feebde42b86d5e7bb
Foka tatsinkou Robert bright <fokrober@e1r11p1.1337.ma>
2019-03-28 23:54:55 +0100

    norme

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -6,14 +6,14 @@
 /*   By: fokrober <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/28 05:32:11 by fokrober          #+#    #+#             */
-/*   Updated: 2019/03/28 23:46:53 by fokrober         ###   ########.fr       */
+/*   Updated: 2019/03/28 23:53:12 by fokrober         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_iterative_factorial(int nb)
 {
 	int i;
-	
+
 	if (nb < 0)
 		return (0);
 	if (nb == 1 || nb == 0)

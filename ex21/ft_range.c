6a45b3f817166c8d48406aecb0a2de3c1426b614
Foka tatsinkou Robert bright <fokrober@e1r9p1.1337.ma>
2019-03-31 21:07:30 +0100

    ft range

---
+++ ex21/ft_range.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fokrober <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/31 15:31:01 by fokrober          #+#    #+#             */
+/*   Updated: 2019/03/31 21:07:02 by fokrober         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		*ft_range(int min, int max)
+{
+	int *range;
+	int	diff;
+
+	diff = max - min;
+	if (diff < 0)
+		return (0);
+	range = (int*)malloc(diff * sizeof(*range));
+	while (--diff)
+		range[diff] = --max;
+	range[diff] = --max;
+	return (range);
+}

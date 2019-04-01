47c4a667f3c71f6c62fb9a281895310799c3bce3
Foka tatsinkou Robert bright <fokrober@e1r9p1.1337.ma>
2019-04-01 06:14:40 +0100

    ft_count_if.c

--- ex26/ft_count_if.c
+++ ex26/ft_count_if.c
@@ -6,16 +6,16 @@
 /*   By: fokrober <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/01 05:08:51 by fokrober          #+#    #+#             */
-/*   Updated: 2019/04/01 05:34:51 by fokrober         ###   ########.fr       */
+/*   Updated: 2019/04/01 06:14:17 by fokrober         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_count_if(char **tab, int(*f)(char*))
+int		ft_count_if(char **tab, int (*f)(char*))
 {
 	int len;
-	
+
 	len = f(*tab);
-	while(*(++tab))
+	while (*(++tab))
 		len += f(*tab);
 	return (len);
 }

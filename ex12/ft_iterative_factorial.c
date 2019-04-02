13e6b57c4948924150d1a73032f3c9b72230573e
Foka tatsinkou Robert bright <fokrober@e1r9p4.1337.ma>
2019-04-02 16:28:54 +0100

    ex12 && ex13 => nb > 13

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: fokrober <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/28 05:32:11 by fokrober          #+#    #+#             */
-/*   Updated: 2019/03/28 23:53:12 by fokrober         ###   ########.fr       */
+/*   Updated: 2019/04/02 16:25:01 by fokrober         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ int		ft_iterative_factorial(int nb)
 {
 	int i;
 
-	if (nb < 0)
+	if (nb < 0 || nb > 13)
 		return (0);
 	if (nb == 1 || nb == 0)
 		return (1);
--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,7 +6,7 @@
 /*   By: fokrober <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/28 05:44:23 by fokrober          #+#    #+#             */
-/*   Updated: 2019/03/28 23:22:42 by fokrober         ###   ########.fr       */
+/*   Updated: 2019/04/02 16:25:22 by fokrober         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ int		ft_recursive_factorial(int nb)
 {
 	if (nb == 1 || nb == 0)
 		return (1);
-	if (nb > 1)
+	if (nb > 1 && nb < 13)
 		return (nb * ft_recursive_factorial(nb - 1));
 	return (0);
 }
